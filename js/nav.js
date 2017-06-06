2a8f8a5df017ef263d9e5a8877e11eded8e5e515
Malika Butler <malika@vimeo.com>
2017-06-06 18:05:43 -0400

    remove console logs from nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,10 @@
 $(function () {
-    console.log("testing");
     var pathname = window.location.pathname;
 
     $linkList = $('.nav__item a');
-    console.log($linkList);
-    console.log(pathname);
 
     $linkList.each(function() {
         if(pathname.indexOf($(this).attr("href")) !== -1) {
-            console.log ($(this));
             $(this).addClass("current");
         }
     });
